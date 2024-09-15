b43f00207fb03e2708c4e56e728d8b9c4c37c2ea
Peter Chung <peterchung1031@gmail.com>
2024-09-15 16:27:30 -0500

    update(components/json-viewer): import json data from file

--- src/routes/components/json-viewer/+page.svelte
+++ src/routes/components/json-viewer/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import JSONViewer from '$lib/ui/json-viewer/json-viewer.svelte'
-	// import plansBenefits from './plans.json'
-	// import testData from './test-data.json'
+	import plansBenefits from './plans.json'
+	import testData from './test-data.json'
 
-	// const benefitId = '1a'
-	// const transformedData = plansBenefits.map((planData) => {
-	// 	const benefit = planData.planBenefits.find((benefit) => benefit.id === benefitId)
-	// 	return { modelFactoryId: planData.modelFactoryId, ...benefit }
-	// })
+	const benefitId = '1a'
+	const transformedData = plansBenefits.map((planData) => {
+		const benefit = planData.planBenefits.find((benefit) => benefit.id === benefitId)
+		return { modelFactoryId: planData.modelFactoryId, ...benefit }
+	})
 </script>
 
 <div class="flex flex-col gap-4">
-	<!-- <div class=" border p-2">
+	<div class=" border p-2">
 		<h2>plansBenefits[0]</h2>
 		<JSONViewer data={plansBenefits[0]} initialOpenDepth={5} />
 	</div>
@@ -29,7 +29,7 @@
 	<div class="border p-2">
 		<h2>testData</h2>
 		<JSONViewer data={testData} />
-	</div> -->
+	</div>
 
 	<div class="border p-2">
 		<h2>Empty Array</h2>
