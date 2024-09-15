5ce160ea238fca503f9cb14539ee7c19c9b20610
Peter Chung <peterchung1031@gmail.com>
2024-09-15 15:30:21 -0500

    fix(ci): use base path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { page } from '$app/stores'
 
 	const { children } = $props()
@@ -13,17 +14,17 @@
 <div class="h-screen w-screen overflow-y-auto">
 	<div class="mx-auto flex max-w-7xl">
 		<nav class="fixed top-0 flex w-48 flex-col overflow-y-auto p-2">
-			<a href="/">Home</a>
+			<a href={`${base}/`}>Home</a>
 			<details open class="accordion my-4 flex flex-col">
 				<summary class="accordion-label font-medium">Components</summary>
 				<div class="accordion-content ml-1 flex flex-col border-l border-gray-500">
-					<a href="/components/button">Button</a>
-					<a href="/components/card">Card</a>
-					<a href="/components/json-viewer">JSON Viewer</a>
+					<a href={`${base}/components/button`}>Button</a>
+					<a href={`${base}/components/card`}>Card</a>
+					<a href={`${base}/components/json-viewer`}>JSON Viewer</a>
 					<a href="/components/json-viewer/etl" class="ml-4">ETL</a>
 				</div>
 			</details>
-			<a href="/repl">REPL</a>
+			<a href={`${base}/repl`}>REPL</a>
 		</nav>
 		<main class="mb-8 w-full flex-grow px-4">
 			<h1 class="mb-2 mt-8 text-4xl font-bold">{$page.data.title}</h1>
